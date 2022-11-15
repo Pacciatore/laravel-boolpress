99eafe4138e3363b63bf8a8abb19a2e11c72f686
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-15 18:58:23 +0100

    predisposizione guest.home

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- CSRF --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- Link CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    {{-- Link script JS --}}
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <title> {{ config('app.name', 'Front Office') }} </title>
+
+</head>
+
+<body>
+    <div id="root">
+
+    </div>
+</body>
+
+</html>
