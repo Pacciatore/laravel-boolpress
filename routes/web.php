4a975cae7deed504296511fefd5b445df564406a
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-16 10:30:19 +0100

    ridefinizione rotte index per template dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,21 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    return view('guest.home');
+})->name('index');
+
+
 Route::middleware('auth')
     ->namespace('Admin')
     ->name('admin.')
     ->prefix('admin')
     ->group(function () {
-        Route::get('/', 'HomeController@index')->name('home');
+        Route::get('/', 'HomeController@index')->name('index');
         Route::resource('posts', 'PostController');
     });
-
-// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('{any?}', function () {
     return view('guest.home');
