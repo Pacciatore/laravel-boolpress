e59d17ea41d5cbd02003dbea9e66cad2c523bb61
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-17 11:36:59 +0100

    associazione category a post con migration

--- app/Post.php
+++ app/Post.php
@@ -8,4 +8,9 @@ class Post extends Model
 {
     //
     protected $fillable = ['title', 'content', 'slug'];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
