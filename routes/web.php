80d3c43e4d6cd6a5adf83adb43427c0ff0f9e971
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-18 10:47:14 +0100

    aggiunta rotta per la risorsa CategoryController

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::middleware('auth')
     ->group(function () {
         Route::get('/', 'HomeController@index')->name('index');
         Route::resource('posts', 'PostController');
+        Route::resource('categories', 'CategoryController');
     });
 
 Route::get('{any?}', function () {
