9a2e68ed6018e017f06f82ab5865b0a5af6e571f
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-18 12:52:46 +0100

    aggiunta di href a edit, index di category in show

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -8,4 +8,13 @@
             {{ $post->title }}
         </a>
     @endforeach
+
+
+    <div class="mt-5">
+        <a href="{{ route('admin.categories.edit', $category->id) }}">Edit Category</a>
+    </div>
+
+    <div>
+        <a href="{{ route('admin.categories.index') }}">Back to Categories</a>
+    </div>
 @endsection
