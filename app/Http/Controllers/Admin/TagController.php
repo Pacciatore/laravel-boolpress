31dd1cdff0c77ba9919889e29bda45e32363bfd8
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-19 13:35:12 +0100

    implementazione destroy crud Tag

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -108,9 +108,13 @@ class TagController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Tag $tag)
     {
         //
+        $tag->posts()->detach();
+        $tag->delete();
+
+        return redirect()->route('admin.tags.index');
     }
 
     private function getSlug($name)
--- resources/views/admin/tags/show.blade.php
+++ resources/views/admin/tags/show.blade.php
@@ -4,12 +4,24 @@
     <h1> {{ $tag->name }} </h1>
 
     @foreach ($tag->posts as $post)
-        <a href="{{ route('admin.posts.show', $post->id) }}"> {{ $post->title }} </a>
+        <p>
+            <a href="{{ route('admin.posts.show', $post->id) }}"> {{ $post->title }} </a>
+        </p>
     @endforeach
 
     {{-- Pulsante per editar il tag --}}
     <div class="mt-5">
         <a class="btn-secondary p-2" href="{{ route('admin.tags.edit', $tag->id) }}">Edit tag</a>
+    </div>
+
+    {{-- Pulsante per eliminare il tag --}}
+    <div class="mt-2">
+        <form onsubmit="return confirm('Are you sure?')" action="{{ route('admin.tags.destroy', $tag->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+
+            <input class="btn-danger" type="submit" value="Delete">
+        </form>
     </div>
 
     {{-- Pulsante per tornare all'index dei tag --}}
