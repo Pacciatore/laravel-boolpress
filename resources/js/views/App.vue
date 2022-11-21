071a565c1495ead84dc005046592cffd6ff8cebd
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-21 19:03:11 +0100

    cambio component in app.vue

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
 
-        <ExampleComponent />
+        <PostsComponent />
 
     </div>
 </template>
 
 <script>
-import ExampleComponent from '../components/ExampleComponent.vue';
+import PostsComponent from '../components/PostsComponent.vue';
 
 
 
 export default {
     name: "App",
-    components: { ExampleComponent }
+    components: { PostsComponent }
 }
 </script>
 
