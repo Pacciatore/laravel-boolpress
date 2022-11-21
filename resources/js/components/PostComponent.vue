aeb7758de7335aa763a0ab73bff49516f14cb10d
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-21 19:50:05 +0100

    visione base dei post e del dettaglio

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+        <h2>{{ post.title }}</h2>
+
+        <div>
+            <p>{{ post.content }}</p>
+        </div>
+
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PostComponent',
+    props: {
+        post: Object
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
