84ccab5dcddc9bf5d5af9a682d897c58f513ffbd
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-23 11:30:44 +0100

    definizione e uso NavComponent

---
+++ resources/js/components/NavComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar">
+        <router-link class="nav-link" to="/">Home</router-link>
+        <router-link class="nav-link" to="/about">About</router-link>
+        <router-link class="nav-link" to="/contacts">Contacts</router-link>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'NavComponent'
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
+
+        <NavComponent />
+
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import NavComponent from '../components/NavComponent.vue';
+
 
 export default {
     name: "App",
+    components: { NavComponent }
 }
 </script>
 
